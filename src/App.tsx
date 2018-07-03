010bfd43fc041cfda5161b0022ce6425238fa66e
lzhao <zhao434@usc.edu>
2018-07-03 13:27:47 -0700

    add css loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
-import './App.css';
+import * as styles from './App.css';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className={styles.app}>
+                <header className={styles.appHeader}>
+                    <img src={logo} className={styles.appLogo} alt="logo" />
+                    <h1 className={styles.appTitle}>Welcome to React</h1>
+                </header>
+                <p className={styles.appIntro}>
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
